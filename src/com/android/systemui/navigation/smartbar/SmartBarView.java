40c47088da2b037d7f31fe62f471691c423b9bf9
ezio84 <brabus84@gmail.com>
2017-03-31 11:01:05 +0200

    Smartbar media arrows: more code improvements
    Change-Id: I5219b16d23fb6bbf6671a36f5c9015c558cd602d
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -176,9 +176,9 @@ public class SmartBarView extends BaseNavigationBar {
         mMediaMonitor = new MediaMonitor(context) {
             @Override
             public void onPlayStateChanged(boolean playing) {
-                if (mImeHintMode == 3) {
+                if (mImeHintMode == 3 && mEditor != null && mEditor.getMode() == BaseEditor.MODE_OFF) {
                     //handle media/ime arrows visibility and smartbutton action type
-                    setNavigationIconHints(mNavigationIconHints, true);
+                    setArrowsMode();
                 }
             }
         };
@@ -386,11 +386,7 @@ public class SmartBarView extends BaseNavigationBar {
                 setImeArrowsVisibility(mCurrentView, View.INVISIBLE);
                 break;
             case IME_AND_MEDIA_HINT_MODE_ARROWS:
-                getImeSwitchButton().setVisibility(View.INVISIBLE);
-                updateCurrentIcons();
-                setImeArrowsVisibility(mCurrentView, (backAlt || (mMediaMonitor.isAnythingPlaying() &&
-                        mAudioManager.isMusicActive())) ? View.VISIBLE : View.INVISIBLE);
-                SmartButtonView.arrowsMediaAction = !backAlt;
+                setArrowsMode();
                 break;
             default: //IME_HINT_MODE_ARROWS
                 getImeSwitchButton().setVisibility(View.INVISIBLE);
@@ -402,6 +398,15 @@ public class SmartBarView extends BaseNavigationBar {
         // Update menu button in case the IME state has changed.
         setMenuVisibility(mShowMenu, true);
         setDisabledFlags(mDisabledFlags, true);
+    }
+
+    private void setArrowsMode() {
+        final boolean backAlt = (mNavigationIconHints & StatusBarManager.NAVIGATION_HINT_BACK_ALT) != 0;
+        getImeSwitchButton().setVisibility(View.INVISIBLE);
+        updateCurrentIcons();
+        setImeArrowsVisibility(mCurrentView, (backAlt || (mMediaMonitor.isAnythingPlaying()
+                && mAudioManager.isMusicActive())) ? View.VISIBLE : View.INVISIBLE);
+        SmartButtonView.arrowsMediaAction = !backAlt;
     }
 
     @Override
